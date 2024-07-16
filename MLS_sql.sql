212b6b1191705a2b7d74cd7f87212a00121f7456
rgr208-usc <80605856+rgr208-usc@users.noreply.github.com>
2024-07-15 22:27:32 -0700

    Update MLS_sql.sql

--- MLS_sql.sql
+++ MLS_sql.sql
@@ -7,10 +7,12 @@ DROP TABLE IF EXISTS TRANS_NUM
 CREATE TABLE TRANS_NUM AS
 (SELECT
     clip, fips_code, listing_address_zip_code,
-    SUBSTRING(close_date_standardized FROM 1 FOR 4) as close_year,
-    SUBSTRING(close_date_standardized FROM 6 FOR 2) as close_month,
+    NULLIF(REGEXP_REPLACE(fips_code, '[^0-9.]+', '', 'g'), '')::numeric AS fips,
+    NULLIF(REGEXP_REPLACE(listing_address_zip_code, '[^0-9.]+', '', 'g'), '')::numeric AS zip,
+    NULLIF(REGEXP_REPLACE(SUBSTRING(listings.close_date_standardized FROM 1 FOR 4),'[^0-9.]+', '', 'g'), '') as close_year,
+    NULLIF(REGEXP_REPLACE(SUBSTRING(listings.close_date_standardized FROM 6 FOR 2),'[^0-9.]+', '', 'g'), '') as close_month,
     NULLIF(REGEXP_REPLACE(SUBSTRING(listing_date FROM 1 FOR 4),'[^0-9.]+', '', 'g'), '') as list_year,
-    SUBSTRING(listing_date FROM 6 FOR 2) as list_month,
+    NULLIF(REGEXP_REPLACE(SUBSTRING(listing_date FROM 6 FOR 2),'[^0-9.]+', '', 'g'), '') as list_month,
     NULLIF(REGEXP_REPLACE(close_price, '[^0-9.]+', '', 'g'), '')::numeric AS price,
     NULLIF(REGEXP_REPLACE(current_listing_price, '[^0-9.]+', '', 'g'), '')::numeric AS list_p,
     NULLIF(REGEXP_REPLACE(original_listing_price, '[^0-9.]+', '', 'g'), '')::numeric AS or_list_p,
@@ -33,6 +35,7 @@ CREATE TABLE zip_mls2 AS
         close_year,
         close_month,
         COUNT(fips_code) AS listings,
+        AVG(fips)     AS fips,
         percentile_cont(0.5) WITHIN GROUP (ORDER BY price) AS price,
         percentile_cont(0.5) WITHIN GROUP (ORDER BY list_p) AS list_p,
         percentile_cont(0.5) WITHIN GROUP (ORDER BY or_list_p) AS or_list_p,
@@ -70,6 +73,25 @@ PROPTY	RI	Residential Income (2-4 units/Duplex/Triplex/Fourplex)
 PROPTY	SF	Single Family
 PROPTY	TH	Townhouse
 PROPTY	TS	Fractional Ownershp/Timeshare
+
+
+*/
+
+/*
+Los Angeles
+
+1. Los Angeles County, California: FIPS code 06037
+2. Orange County, California: FIPS code 06059
+3. Riverside County, California: FIPS code 06065
+4. San Bernardino County, California: FIPS code 06071
+5. Ventura County, California: FIPS code 06111
+
+
+1. San Francisco County, California: FIPS code 06075
+2. Alameda County, California: FIPS code 06001
+3. Contra Costa County, California: FIPS code 06013
+4. San Mateo County, California: FIPS code 06081
+5. Marin County, California: FIPS code 06041
 
 
 */
