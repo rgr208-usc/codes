b1a03465130a0743ba293c42527286cff9cd8839
rgr208-usc <80605856+rgr208-usc@users.noreply.github.com>
2024-07-15 22:01:54 -0700

    Update MLS_sql.sql

--- MLS_sql.sql
+++ MLS_sql.sql
@@ -1,47 +1,38 @@
 
-
 ---FULL TABLE ----
-DROP TABLE transaction
-CREATE TABLE transaction AS
-(SELECT clip, fips_code, listing_address_zip_code, SUBSTRING(close_date_standardized FROM 1 FOR 4) as close_year,
-        SUBSTRING(close_date_standardized FROM 6 FOR 2) as close_month,
-        close_price, current_listing_price, original_listing_price, price_per_square_foot,
-        days_on_market_dom_derived, days_on_market_dom_cumulative, property_type_code_standardized
-        FROM mls.listings
-WHERE listing_status_category_code_standardized='S'  AND (fips_code='06037' OR fips_code='06059' OR  fips_code='06065' OR  fips_code='06071'  OR fips_code='06111'));
 
---Numeric Conversation
+-- LA (fips_code='06037' OR fips_code='06059' OR  fips_code='06065' OR  fips_code='06071'  OR fips_code='06111'));
 
--- 'clip', 'fips_code', 'listing_address_zip_code','close_year','close_month','close_price', 'current_listing_price', 'original_listing_price', 'price_per_square_foot',
---         'days_on_market_dom_derived', 'days_on_market_dom_cumulative'
-
-DO $$
-DECLARE
-    cols TEXT[] := ARRAY['clip', 'fips_code', 'listing_address_zip_code','close_year','close_month','close_price', 'current_listing_price', 'original_listing_price', 'price_per_square_foot',
- 'days_on_market_dom_derived', 'days_on_market_dom_cumulative']; -- List your existing columns here
-    col TEXT;
-BEGIN
-    FOREACH col IN ARRAY cols
-    LOOP
-        -- Step 1: Add the new column
-        EXECUTE format('ALTER TABLE transaction ADD COLUMN %I_n numeric', col);
-
-        -- Step 2: Update the new column with valid numeric values
-        EXECUTE format('UPDATE transaction SET %I_n = CASE
-            WHEN %I ~ ''^[+-]?[0-9]+(\.[0-9]+)?$'' THEN %I::numeric
-            ELSE NULL
-        END', col, col, col);
-    END LOOP;
-END $$;
+DROP TABLE TRANS_NUM
+CREATE TABLE TRANS_NUM AS
+(SELECT
+    clip, fips_code,  listing_address_zip_code,  property_type_code_standardized,
+    SUBSTRING(close_date_standardized FROM 1 FOR 4) as close_year,
+    SUBSTRING(close_date_standardized FROM 6 FOR 2) as close_month,
+    NULLIF(REGEXP_REPLACE(SUBSTRING(listing_date FROM 1 FOR 4),'[^0-9.]+', '', 'g'), '') as list_year,
+    SUBSTRING(listing_date FROM 6 FOR 2) as list_month,
+    NULLIF(REGEXP_REPLACE(close_price, '[^0-9.]+', '', 'g'), '')::numeric AS price,
+    NULLIF(REGEXP_REPLACE(current_listing_price, '[^0-9.]+', '', 'g'), '')::numeric AS list_p,
+    NULLIF(REGEXP_REPLACE(original_listing_price, '[^0-9.]+', '', 'g'), '')::numeric AS or_list_p,
+    NULLIF(REGEXP_REPLACE(price_per_square_foot, '[^0-9.]+', '', 'g'), '')::numeric AS list_ppsf,
+    NULLIF(REGEXP_REPLACE(days_on_market_dom_derived, '[^0-9.]+', '', 'g'), '')::numeric AS dom,
+    NULLIF(REGEXP_REPLACE(days_on_market_dom_cumulative, '[^0-9.]+', '', 'g'), '')::numeric AS cumdom
+FROM   mls.listings
+WHERE listing_status_category_code_standardized='S');
 
 
 ---collpase----
 
+--issue with median
+
+--conditional
+
 DROP TABLE zip_mls
-CREATE TABLE zip_mls AS
-(SELECT listing_address_zip_code, close_year, close_month, COUNT(fips_code) as listings, AVG( price_per_square_foot_n) AS list_psf,
-AVG(current_listing_price_n) AS list_p, AVG(close_price_n) AS price,AVG(days_on_market_dom_derived_n) AS dom
- FROM transaction
-                                      GROUP BY listing_address_zip_code, listing_year,listing_month ORDER BY listing_address_zip_code, listing_year,listing_month ) ;
+CREATE TABLE zip_mls2 AS
+(SELECT listing_address_zip_code, close_year, close_month, COUNT(fips_code) as listings, AVG( price) AS price , AVG(list_p) as list_p,
+AVG(or_list_p) AS or_list_p, AVG(price*list_ppsf/NULLIF(list_p, 0)) as ppsf , AVG(dom) AS dom, AVG(cumdom) AS cumdom
+ FROM TRANS_NUM
+                                      GROUP BY listing_address_zip_code, close_year, close_month ORDER BY listing_address_zip_code, close_year, close_month ) ;
 
 
+SELECT * FROM zip_ml
