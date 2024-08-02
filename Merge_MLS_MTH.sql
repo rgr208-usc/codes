9ad3f6ec76a0ee16aca109ff554a67794bc39249
rgr208-usc <80605856+rgr208-usc@users.noreply.github.com>
2024-08-01 17:58:38 -0700

    Create Merge_MLS_MTH.sql

---
+++ Merge_MLS_MTH.sql
@@ -0,0 +1,63 @@
+
+--MERGING ON DATE RANGE
+
+
+DROP TABLE IF EXISTS MLS;
+CREATE TABLE MLS AS
+SELECT
+    clip as clip_mls, fips_code as fips_mls, SUBSTRING(listing_address_zip_code FROM 1 FOR 5) as listing_address_zip_code ,
+    listing_status_category_code_standardized,property_type_code_standardized,
+    TO_DATE(SUBSTRING(close_date_standardized FROM 1 FOR 10), 'YYYY-MM-DD') AS closedate,
+    TO_DATE(SUBSTRING(listing_date FROM 1 FOR 10), 'YYYY-MM-DD') AS listing_date,
+    NULLIF(REGEXP_REPLACE(fips_code, '[^0-9.]+', '', 'g'), '')::numeric AS fips,
+    NULLIF(REGEXP_REPLACE(listing_address_zip_code, '[^0-9.]+', '', 'g'), '')::numeric AS zip,
+    NULLIF(REGEXP_REPLACE(SUBSTRING(listings.close_date_standardized FROM 1 FOR 4),'[^0-9.]+', '', 'g'), '')::integer as year,
+    NULLIF(REGEXP_REPLACE(SUBSTRING(listings.close_date_standardized FROM 6 FOR 2),'[^0-9.]+', '', 'g'), '')::integer as month,
+    NULLIF(REGEXP_REPLACE(SUBSTRING(listings.close_date_standardized FROM 9 FOR 2),'[^0-9.]+', '', 'g'), '')::integer as day,
+    NULLIF(REGEXP_REPLACE(SUBSTRING(listing_date FROM 1 FOR 4),'[^0-9.]+', '', 'g'), '') as list_year,
+    NULLIF(REGEXP_REPLACE(SUBSTRING(listing_date FROM 6 FOR 2),'[^0-9.]+', '', 'g'), '') as list_month,
+    NULLIF(REGEXP_REPLACE(close_price, '[^0-9.]+', '', 'g'), '')::numeric AS price,
+    NULLIF(REGEXP_REPLACE(current_listing_price, '[^0-9.]+', '', 'g'), '')::numeric AS list_p,
+    NULLIF(REGEXP_REPLACE(original_listing_price, '[^0-9.]+', '', 'g'), '')::numeric AS or_list_p,
+    NULLIF(REGEXP_REPLACE(price_per_square_foot, '[^0-9.]+', '', 'g'), '')::numeric AS list_ppsf,
+    NULLIF(REGEXP_REPLACE(days_on_market_dom_derived, '[^0-9.]+', '', 'g'), '')::numeric AS dom,
+    NULLIF(REGEXP_REPLACE(days_on_market_dom_cumulative, '[^0-9.]+', '', 'g'), '')::numeric AS cumdom
+    FROM mls.listings
+WHERE listing_status_category_code_standardized='S' AND
+      --choose the type of property
+      (property_type_code_standardized='CN' OR property_type_code_standardized='SF' OR  property_type_code_standardized='TH'   )
+;
+
+DROP TABLE IF EXISTS MTG;
+CREATE TABLE MTG AS
+SELECT clip as clipm,
+       TO_DATE( TO_CHAR(TO_DATE(mortgage_recording_date, 'YYYYMMDD'), 'YYYY-MM-DD'),'YYYY-MM-DD')  AS mtgdate,
+       mortgage_type_code, --PURCHASE, REFI, JUNIOR, P,R,J
+       mortgage_purpose_code,  --F (First Mortgage), see below
+       conforming_loan_indicator,conventional_loan_indicator, refinance_loan_indicator,
+       government_sponsored_enterprise_gse_eligible_mortgage_indicator,
+        construction_loan_indicator,equity_loan_indicator,fha_loan_indicator,veterans_administration_loan_indicator,
+        multifamily_rider_indicator,condominium_rider_indicator,second_home_rider_indicator,
+        variable_rate_loan_indicator, fixed_rate_indicator,
+            NULLIF(REGEXP_REPLACE(mortgage.basics.fips_code, '[^0-9.]+', '', 'g'), '') ::numeric AS fips,
+            NULLIF(REGEXP_REPLACE(fixed_rate_indicator, '[^0-9.]+', '', 'g'), '') ::numeric AS fix,
+            NULLIF(REGEXP_REPLACE(mortgage_amount, '[^0-9.]+', '', 'g'), '') ::numeric AS amount,
+            NULLIF(REGEXP_REPLACE(SUBSTRING(mortgage_recording_date FROM 1 FOR 4),'[^0-9.]+', '', 'g'), '')::integer AS year,
+            NULLIF(REGEXP_REPLACE(SUBSTRING(mortgage_recording_date FROM 5 FOR 2),'[^0-9.]+', '', 'g'), '')::integer  AS month,
+            NULLIF(REGEXP_REPLACE(SUBSTRING(mortgage_recording_date FROM 7 FOR 2),'[^0-9.]+', '', 'g'), '')::integer  AS day,
+            NULLIF(REGEXP_REPLACE(mortgage_interest_rate, '[^0-9.]+', '', 'g'), '') ::numeric AS rate
+    FROM mortgage.basics
+   WHERE property_indicator_code___static IN ('10', '11', '21', '22') AND  mortgage_type_code = 'P'
+;
+
+---MERGE
+
+DROP TABLE IF EXISTS MLS_MTG;
+SELECT *
+FROM MLS
+LEFT JOIN MTG
+ON MLS.clip_mls = MTG.clipm AND
+ABS(DATE_PART('day', MLS.closedate - MTG.mtgdate)) <= 5;
+
+
+
